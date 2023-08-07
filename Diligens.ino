cbb63ebfeb81193d59bc0bd1bf477e700d58bef9
VitorTavaresO <vitortavares.o55@gmail.com>
2023-08-07 19:42:38 -0300

    Blink

--- Diligens.ino
+++ Diligens.ino
@@ -1,9 +1,10 @@
 void setup() {
+  pinMode(15, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(500);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(500);                      // wait for a second
+  digitalWrite(15, HIGH);  
+  delay(500);
+  digitalWrite(15, LOW);
+  delay(500);
 }
