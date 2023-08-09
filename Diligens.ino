b4762cd1d9137820cbe10056a331d3e3e4391ad1
VitorTavaresO <vitortavares.o55@gmail.com>
2023-08-09 14:15:49 -0300

    Star LCD

--- Diligens.ino
+++ Diligens.ino
@@ -1,10 +1,16 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+#define HUMIDITY 2
+
 void setup() {
-  pinMode(2, INPUT);
+  pinMode(HUMIDITY, INPUT);
   Serial.begin(115200);
+  lcd.begin (20,4);
 }
 
 void loop() {
-  int val = analogRead(2);
+  int val = analogRead(HUMIDITY);
   Serial.println(val);
-  delay(1000);
+  delay(60000);
 }
