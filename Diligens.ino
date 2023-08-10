a73b6c185cbed560b53486451cccdf3f68507107
VitorTavaresO <vitortavares.o55@gmail.com>
2023-08-10 12:36:27 -0300

    LCD 20x40

--- Diligens.ino
+++ Diligens.ino
@@ -1,12 +1,22 @@
 #include <LiquidCrystal_I2C.h>
-#include <Wire.h>
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 #define HUMIDITY 2
 
 void setup() {
   pinMode(HUMIDITY, INPUT);
   Serial.begin(115200);
-  lcd.begin (20,4);
+  lcd.init();
+  lcd.clear();         
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Humidity: ");
+  lcd.setCursor(0,1);
+  lcd.print("Temperature: ");
+  lcd.setCursor(0,2);
+  lcd.print("Last Sunbath: ");
+  lcd.setCursor(0,3);
+  lcd.print("Last Attention: ");
+
 }
 
 void loop() {
