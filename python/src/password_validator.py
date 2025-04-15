73ced6c3ebce147d970b104294d4560786a72684
VitorTavaresO <vitortavares.o55@gmail.com>
2025-04-15 20:33:40 -0300

    Using RegEX on the code

--- python/src/password_validator.py
+++ python/src/password_validator.py
@@ -1,34 +1,23 @@
+import re
+
 class PasswordValidator:
     def __init__(self):
         self.erros = []
+        self.validation_patterns = [
+            (re.compile(r'.{8,}'), "The password must be at least 8 characters long"),
+            (re.compile(r'[A-Z]'), "The password must contain at least one uppercase letter"),
+            (re.compile(r'[a-z]'), "The password must contain at least one lowercase letter"),
+            (re.compile(r'[0-9]'), "The password must contain at least one number"),
+            (re.compile(r'[!@#$%^&*()\-_=+\[\]{};:,.<>?/]'), "The password must contain at least one special character"),
+            (re.compile(r'^[^ ]+$'), "The password must not contain blank spaces")
+        ]
 
     def get_errors(self):
         return self.erros
 
     def validate(self, password):
-        if len(password) < 8:
-            self.erros.append("The password must be at least 8 characters long")
-            return False
-       
-        elif not any(char.isupper() for char in password):
-            self.erros.append("The password must contain at least one uppercase letter")
-            return False
-       
-        elif not any(char.islower() for char in password):
-            self.erros.append("The password must contain at least one lowercase letter")
-            return False
-        
-        elif not any(char.isdigit() for char in password):
-            self.erros.append("The password must contain at least one number")
-            return False
-        
-        elif not any(char in "!@#$%^&*()-_=+[]{};:,.<>?/" for char in password):
-            self.erros.append("The password must contain at least one special character")
-            return False
-        
-        elif " " in password:
-            self.erros.append("The password must not contain blank spaces")
-            return False
-        
-        return True
-    
+        self.erros = []
+        for pattern, error_msg in self.validation_patterns:
+            if not pattern.search(password):
+                self.erros.append(error_msg)
+        return len(self.erros) == 0
